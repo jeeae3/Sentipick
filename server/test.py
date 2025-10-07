390e9cd161a57ee110443ccd8d232b06610c00c6
Jeeae <129932495+jeeae3@users.noreply.github.com>
2025-10-07 17:21:35 -0400

    update

--- server/test.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoTokenizer
-from transformers import AutoModelForSequenceClassification
-from scipy.special import softmax
-
-MODEL = f"cardiffnlp/twitter-roberta-base-sentiment"
-
-tokenizer = AutoTokenizer.from_pretrained(MODEL)
-model = AutoModelForSequenceClassification.from_pretrained(MODEL)
